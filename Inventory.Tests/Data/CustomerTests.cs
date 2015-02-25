1441924cd449f5320cd1652ab7e60f338ccb7b39
Reka Burmeister <reka.burmeister@gmail.com>
2015-02-25 22:32:12 +0000

    Checking in first failing test: simple create customer. Customer has a string username and a string password. Although protecting the password doesn't seem to be a requirement on its own I still feel like the customer's password should never be exposed. I didn't implement any encryption of the password as it's not a requirement.

---
+++ Inventory.Tests/Data/CustomerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Inventory.Tests.Data
+{
+    [TestClass]
+    public class CustomerTests
+    {
+        [TestMethod]
+        public void CreateCustomer()
+        {
+            const string customerName = "New Customer";
+            const string customerPassword = "password";
+            Customer customer = new Customer(customerName, customerPassword);
+            Assert.AreEqual(customerName, customer.Name, "Customer name does not match");
+            Assert.IsTrue(customer.HasPassword(customerPassword), "Customer password is not as expected");
+        }
+    }
+}
