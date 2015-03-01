5dc33f611c8121f0ba1345b6b848e94df3db5786
Reka Burmeister <reka.burmeister@gmail.com>
2015-03-01 23:34:10 +0000

    Class for service tests

---
+++ Inventory.Tests/ServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Inventory.ServiceLayer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace Inventory.Tests
+{
+    [TestFixture]
+    public class ServiceTests
+    {
+        private MerchantDatabase m_MerchantDatabase;
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            m_MerchantDatabase = new MerchantDatabase();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            m_MerchantDatabase = new MerchantDatabase();    
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
