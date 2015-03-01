7a58e5d696bc2dcac1ef264f8f4a436a48df048f
Reka Burmeister <reka.burmeister@gmail.com>
2015-03-01 23:04:24 +0000

    The database representation should really be in the service layer

--- Inventory/Inventory.ServiceLayer/MerchantDatabase.cs
+++ Inventory/Inventory.ServiceLayer/MerchantDatabase.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Inventory.Data;
 
-namespace Inventory.Data
+namespace Inventory.ServiceLayer
 {
     public class MerchantDatabase
     {
