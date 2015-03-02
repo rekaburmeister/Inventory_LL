6a23a113e5c5bcbcbf261ed5b6a13112fede3fa8
Reka Burmeister <reka.burmeister@gmail.com>
2015-03-02 01:37:19 +0000

    Objects that control the purchase and the final tests

---
+++ Inventory.Tests/PurchaseTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace Inventory.Tests
+{
+    [TestFixture]
+    public class PurchaseTests
+    {
+        [Test]
+        public void CustomerLogin()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void PurchaseItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void UpdateSupplierStock()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void FailoverSupplier()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void SecondaryFailoverSupplier()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void CreateInvoice()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void ProfitAndLoss()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void CountOfStockPerItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void ListOfItemsPerCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void ListOfItemsFromSupplier()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void ListOfSuppliersOfItem()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Test]
+        public void OutOfStockWarning()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
