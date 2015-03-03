f4aa2a7a0ddb3a0d94dba3bfba656e244b2c8d17
Reka Burmeister <reka.burmeister@gmail.com>
2015-03-03 17:57:29 +0000

    Customer class

---
+++ Inventory.Tests/Roles/CustomerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inventory.Roles;
+using NUnit.Framework;
+
+namespace Inventory.Tests.Roles
+{
+    [TestFixture]
+    class CustomerTests
+    {
+        [TestCase("John", "Doe", "john.doe", "JohnDoe11")]
+        public void CreateCustomer(string firstName, string surname, string userName, string password)
+        {
+            var customer = new Customer(firstName, surname, userName, password);
+            Assert.AreEqual(firstName, customer.FirstName, "First name was set correctly");
+            Assert.AreEqual(surname, customer.Surname, "Surname was set correctly");
+            Assert.AreEqual(userName, customer.UserName, "UserName was set correctly");
+            Assert.AreEqual(password, customer.Password, "Password was set correctly");
+            Assert.AreEqual(0.0, customer.Balance, "Default balance is 0");
+        }
+    }
+}
---
+++ Inventory/Inventory.Roles/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Roles
+{
+    public class Customer
+    {
+        public Customer(string firstName, string surname, string userName, string password)
+        {
+            FirstName = firstName;
+            Surname = surname;
+            UserName = userName;
+            Password = password;
+            Balance = 0.0;
+        }
+
+        public string FirstName { get; private set; }
+        public string Surname { get; private set; }
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+        public double Balance { get; private set; }
+    }
+}
